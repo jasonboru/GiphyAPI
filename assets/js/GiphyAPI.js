8c5b811d5e8f7c50245c1f1154e4adad6bcc9082
jasonboru <jasonboru@gmail.com>
2017-03-08 06:47:33 -0500

    buttons rendering properly

--- assets/js/GiphyAPI.js
+++ assets/js/GiphyAPI.js
@@ -4,9 +4,11 @@ var topics = ["The Simpsons", "Rick and Morty", "Family Guy",
 
 function renderButtons() { //function to go through array and render the buttons to the DOM
 
+	$("#buttons").empty(); //clear old buttons before re-redering
+
     for (var i = 0; i < topics.length; i++) { // for loop to go through the whole array
-    	
-      	$("#buttons").append($("<button>") //append a button tag to the div with id of buttons
+
+    	$("#buttons").append($("<button>") //append a button tag to the div with id of buttons
 	      	.text(topics[i])) // puts the array title of the current index the for loop is working through
 	      	.addClass("btn btn-default tvShowBtn"); // gives that button some bootstrap classes and a class of my own
 	      	    }
@@ -24,6 +26,7 @@ $(document).on("click", "#submit-button", function() { //document click event on
 	$("#search-term").val(''); //clear the search field after a submit by the user
 	topics.push(searchInput); // push the searchInput into the topics array
 	console.log(topics);
+	renderButtons([topics]);
 });
 
 $(document).ready(function() { //when the document loads
